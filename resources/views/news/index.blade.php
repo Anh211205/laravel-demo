7a2a84e3804cebc279f9f73392bd2d37f95c0023
Anh Phung Thi Ngoc <23010148@st.phenikaa-uni.edu.vn>
2026-02-01 19:54:49 +0700

    Update project

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,23 +1,44 @@
-@extends('layouts.myapp')	
-@section('content')	
-<h1>Danh sách tin tức</h1>	
-<a	href="{{route('news.create')}}">Thêm tin tức mới</a>	
-<ul>	
-@foreach($news as $item)	
-<li>	
-{{$item->title}}
-<a	href="{{route('news.show',$item->id)}}">{{$item->title}}</a>	
-<a	href="{{route('news.edit',$item->id)}}">Sửa</a>	
-<form action="{{route('news.destroy',$item->id)}}"	method="POST"	
-style="display:inline">	
-@csrf	
-@method('DELETE')	
-<button	type="submit">Xóa</button>	
-</form>	
-</li>	
-@endforeach	
+@extends('layouts.myapp')
+
+@section('content')
+<h1>Danh sách tin tức</h1>
+
+@auth
+    <a href="{{ route('news.create') }}" class="btn btn-primary">
+        Thêm tin mới
+    </a>
+@endauth
+
+<ul>
+@foreach($news as $item)
+    <li>
+        <h3>
+            <a href="{{ route('news.show', $item->id) }}">
+                {{ $item->title }}
+            </a>
+        </h3>
+
+        @if(!empty($item->summary))
+            <p>{{ $item->summary }}</p>
+        @endif
+
+        @auth
+            <a href="{{ route('news.edit', $item->id) }}">Sửa</a>
+
+            <form action="{{ route('news.destroy', $item->id) }}"
+                  method="POST"
+                  style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Xóa</button>
+            </form>
+        @endauth
+    </li>
+@endforeach
 </ul>
+
 <div>
-	{{$news->links()}}
+    {{ $news->links() }}
 </div>
 @endsection
+
--- routes/news.php
+++ routes/news.php
@@ -2,12 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
-use	App\Http\Controllers\CommentController;	
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return redirect()->route('news.index');
 })->name('home');
 
-Route::resource('news', NewsController::class);
-Route::post('news/{news}/comments',	[CommentController::class,	'store'])
-->name('news.comments.store');	
+// Comment (bắt buộc đăng nhập)
+Route::post('news/{news}/comments', [CommentController::class, 'store'])
+    ->middleware('auth')
+    ->name('news.comments.store');
+
+// Bảo vệ thêm / sửa / xóa (L14)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
+    Route::post('/news', [NewsController::class, 'store'])->name('news.store');
+    Route::get('/news/{id}/edit', [NewsController::class, 'edit'])->name('news.edit');
+    Route::put('/news/{id}', [NewsController::class, 'update'])->name('news.update');
+    Route::delete('/news/{id}', [NewsController::class, 'destroy'])->name('news.destroy');
+});
+
+// Public routes
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news/{id}', [NewsController::class, 'show'])->name('news.show');
